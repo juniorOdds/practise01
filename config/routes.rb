2b77ec70564a5679e71f0462018a5cbe25d15291
my_macbook <my_macbook@my-macbooks-MacBook-Pro.local>
2024-05-15 17:02:09 +0700

    update kub

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # root"friendlists#index"
   root'pages#home'
-  get'/home',to 
 
   # Defines the root path route ("/")
   # root "posts#index"
