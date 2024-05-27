933f75edc54c8ba39c04fef96994246d7a056e3b
my_macbook <my_macbook@MacBook-Air.local>
2024-05-27 15:15:04 +0700

    ซ้อมใช้ git

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    
+  end
+
+  def sign_in
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :friendlists
-  resources :friends
+  # devise_for :users
+  # resources :friendlists
+  # resources :friends
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # root"friendlists#index"
-  root'pages#home'
+  # root 'pages#home'
+  root "pages#home"
+
+  get "sign_in", to:"session#create"
+
+  # get "sign_in" , to:]
 
   # Defines the root path route ("/")
   # root "posts#index"
